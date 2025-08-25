f3bb97925c66ba133f62105a769dbf286c85205d
Rupesh Malisetty <mrupesh2005@gmail.com>
2025-08-25 10:45:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div className="LeftSide">
+        <div className="Intro">
+          <img
+            src="./assests/square.png"
+            alt="Profile Picture"
+            className="ProfilePic"
+          />
+        </div>
+      </div>
+      <div className="RightSide"></div>
+    </>
+  );
+}
+
+export default App;
